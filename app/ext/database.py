a105a96abba8fdd0ac5f2cd783af70dc2dd298a0
Jônatas Oliveira <contato@jonatasoliveira.me>
2020-10-27 20:55:38 -0300

    init commit

---
+++ app/ext/database.py
@@ -0,0 +1,23 @@
+from tortoise import Tortoise
+from dynaconf import settings
+
+async def init():
+    # Here we connect to a Postgres.
+    # also specify the app name of "models"
+    # which contain models from "app.models"
+    await Tortoise.init(
+        db_url=settings.DATABASE_URL,
+        modules={'models': ['app.models', "aerich.models"]}
+    )
+
+
+TORTOISE_ORM = {
+    "connections": {"default":
+    "postgres://partyoudbuser:partyou123@localhost/sapu"},
+    "apps": {
+        "models": {
+            "models": ["models", "aerich.models"],
+            "default_connection": "default",
+        },
+    },
+}
---
+++ app/models/user_model.py
@@ -0,0 +1,15 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    document = fields.TextField
+    mail = fields.TextField
+    oauth = fields.TextField
+    role_id = fields.IntField
+
+    def __str__(self):
+        return self.name
+
